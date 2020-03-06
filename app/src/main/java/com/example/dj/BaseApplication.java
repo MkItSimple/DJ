9eda7cf2ab8d799c53bee8db6b9e2fa21075b61e
MkItSimple <choanania1@gmail.com>
2020-03-06 08:36:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dj/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dj;
+
+import com.example.dj.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
